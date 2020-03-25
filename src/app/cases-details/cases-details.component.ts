6c5ffd3feed2a1be25c74b01033465311a82cd51
Alex <abiz@ucsd.edu>
2020-03-25 13:58:39 -0700

    step #5 almost done

--- src/app/cases-details/cases-details.component.ts
+++ src/app/cases-details/cases-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Cases } from '../cases';
 
 @Component({
   selector: 'app-cases-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasesDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
+  cases: Cases = {_id: '', name: '', gender: '', age: null, address: '', 
+                  city: '', country: '', status: '', updated: null};
+  isLoadingResults = true;
+  getCasesDetails(id: string){
+    this.api.getCasesById(id)
+      .subscribe((data: any) => {
+        this.cases = data;
+        console.log(this.cases);
+        this.isLoadingResults = false;
+      });
+  }
   ngOnInit(): void {
+    this.getCasesDetails(this.route.snapshot.params.id);
   }
 
 }
